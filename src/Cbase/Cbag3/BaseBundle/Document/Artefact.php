db64ce1a0a56f6e8fdad37710255a43938782637
dazz <dazz@c-base.org>
2012-08-22 19:44:32 +0200

    added mapping information for Artefact

--- src/Cbase/Cbag3/BaseBundle/Document/Artefact.php
+++ src/Cbase/Cbag3/BaseBundle/Document/Artefact.php
@@ -2,7 +2,20 @@
 
 namespace Cbase\Cbag3\BaseBundle\Document;
 
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * @MongoDB\Document
+ */
 class Artefact
 {
+    /**
+     * @MongoDB\Id
+     */
+    protected $id;
+
+    /**
+     * @MongoDB\String
+     */
     protected $name;
 }
