ed249450c646729f7f310aa874bdaaca8356501e
dazz <dazz@c-base.org>
2012-08-24 10:54:36 +0200

    created artefact form type

---
+++ src/Cbase/Cbag3/BaseBundle/Form/Type/ArtefactType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Cbase\Cbag3\BaseBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ArtefactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+    }
+
+    public function getDefaultOptions(array $options)
+    {
+        return array('data_class' => 'Cbase\Cbag3\BaseBundle\Document\Artefact');
+    }
+
+    public function getName()
+    {
+        return 'artefact';
+    }
+}
