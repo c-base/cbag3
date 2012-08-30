00152564d4e4a3d5c01c867b28ea738f2357b33b
dazz <dazz@c-base.org>
2012-08-30 16:22:20 +0200

    added fields createdBy, createdAt, slug and state

--- src/Cbase/Cbag3/BaseBundle/Document/Artefact.php
+++ src/Cbase/Cbag3/BaseBundle/Document/Artefact.php
@@ -5,6 +5,8 @@ namespace Cbase\Cbag3\BaseBundle\Document;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 use Cbase\Cbag3\BaseBundle\Document\Asset;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * @MongoDB\Document
  * @MongoDB\Document(repositoryClass="Cbase\Cbag3\BaseBundle\Repository\ArtefactRepository")
@@ -31,10 +33,40 @@ class Artefact
      */
     protected $assets = array();
 
+    /**
+     * @MongoDB\String
+     */
+    protected $slug;
+
+    /**
+     * @MongoDB\Date
+     */
+    protected $createdAt;
+
+
+    /**
+     * @MongoDB\String
+     */
+    protected $createdBy;
+
+    /**
+     * @MongoDB\EmbedOne(targetDocument="ArtefactState")
+     */
+    protected $state;
+
 
     public function __construct()
     {
         $this->assets = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * @MongoDB\PrePersist()
+     */
+    protected function prePersist()
+    {
+        $this->createdAt = new \Date();
+        $this->createdBy = "alien";
     }
 
     /**
@@ -109,5 +141,93 @@ class Artefact
     public function getAssets()
     {
         return $this->assets;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return Artefact
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string $slug
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param date $createdAt
+     * @return Artefact
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return date $createdAt
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param string $createdBy
+     * @return Artefact
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return string $createdBy
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * Set state
+     *
+     * @param Cbase\Cbag3\BaseBundle\Document\ArtefactState $state
+     * @return Artefact
+     */
+    public function setState(\Cbase\Cbag3\BaseBundle\Document\ArtefactState $state)
+    {
+        $this->state = $state;
+        return $this;
+    }
+
+    /**
+     * Get state
+     *
+     * @return Cbase\Cbag3\BaseBundle\Document\ArtefactState $state
+     */
+    public function getState()
+    {
+        return $this->state;
     }
 }
