3c4ef22ccbbe54242eb90fc1ba9104204192eda4
dazz <dazz@c-base.org>
2012-08-31 19:16:47 +0200

    added ArtefactBundle

---
+++ src/Cbase/Cbag3/ArtefactBundle/CbaseCbag3ArtefactBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Cbase\Cbag3\ArtefactBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CbaseCbag3ArtefactBundle extends Bundle
+{
+}
---
+++ src/Cbase/Cbag3/ArtefactBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Cbase\Cbag3\ArtefactBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/hello/Fabien');
+
+        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    }
+}
