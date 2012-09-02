00ca98c185321153b83729cd3f6f2636a7b5b415
dazz <dazz@c-base.org>
2012-09-02 03:07:08 +0200

    not all testet, but show, index and edit are working

--- src/Cbase/Cbag3/ArtefactBundle/Controller/ArtefactController.php
+++ src/Cbase/Cbag3/ArtefactBundle/Controller/ArtefactController.php
@@ -3,8 +3,11 @@
 namespace Cbase\Cbag3\ArtefactBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
 use Symfony\Component\HttpFoundation\Response;
 
 use Cbase\Cbag3\ArtefactBundle\Document\Artefact;
@@ -16,6 +19,7 @@ class ArtefactController extends Controller
     /**
      * @Route("/")
      * @Template()
+     * @Method("GET")
      */
     public function indexAction()
     {
@@ -24,8 +28,20 @@ class ArtefactController extends Controller
     }
 
     /**
-     * @Route("/create")
+     * @Route("/new")
      * @Template()
+     * @Method("GET")
+     */
+    public function newAction()
+    {
+        $form = $this->createForm(new ArtefactType(), new Artefact());
+        return array('form'=>$form->createView());
+    }
+
+    /**
+     * @Route("/")
+     * @Template()
+     * @Method("POST")
      *
      * @return Response
      */
@@ -33,81 +49,74 @@ class ArtefactController extends Controller
     {
         $form = $this->createForm(new ArtefactType(), new Artefact());
 
-        if ($this->getRequest()->getMethod() == 'POST') {
-            $form->bind($this->getRequest());
-
-            if ($form->isValid()) {
-                // perform some action, such as saving the task to the database
-                $artefact = $form->getData();
-
-                $dm = $this->get('doctrine.odm.mongodb.document_manager');
-                $dm->persist($artefact);
-                $dm->flush();
-
-                $id = $artefact->getId();
-
-                return $this->redirect($this->generateUrl('cbase_cbag3_artefact_artefact_show', array('id'=> $id)));
-            }
+        if ($slug = $this->processArtefactForm($form) !== false) {
+            return $this->redirect($this->generateUrl('cbase_cbag3_artefact_artefact_show', array('slug'=> $slug)));
         }
 
         return array('form'=>$form->createView());
     }
 
     /**
-     * @Route("/show/{id}")
+     * @Route("/{slug}")
      * @Template()
-     *
-     * @param $id
-     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     * @Method("GET")
      */
-    public function showAction($id)
+    public function showAction($slug)
     {
         $artefact = $this->getArtefactRepository()
-            ->find($id);
+            ->findOneBySlug($slug);
 
         if (!$artefact) {
-            throw $this->createNotFoundException('No artefact found for id '.$id);
+            throw $this->createNotFoundException('No artefact found for '.$slug);
         }
 
         return array('artefact'=>$artefact);
     }
 
     /**
-     * @Route("/update/{id}")
+     * @Route("/{slug}/edit")
      * @Template()
+     * @Method("GET")
      *
-     * @param $id
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
      */
-    public function updateAction($id)
+    public function editAction($slug)
     {
         $dm = $this->get('doctrine.odm.mongodb.document_manager');
 
-        $artefact = $dm->getRepository('CbaseCbag3ArtefactBundle:Artefact')->find($id);
+        $artefact = $this->getArtefactRepository()
+            ->findOneBySlug($slug);
 
         if (!$artefact) {
-            throw $this->createNotFoundException('No artefact found for id '.$id);
+            throw $this->createNotFoundException('No artefact found for '.$slug);
         }
 
         $form = $this->createForm(new ArtefactType(), $artefact);
 
-        if ($this->getRequest()->getMethod() == 'POST') {
-            $form->bind($this->getRequest());
+        return array('form'=>$form->createView(), 'slug'=>$slug);
+    }
 
-            if ($form->isValid()) {
-                // perform some action, such as saving the task to the database
-                $artefact = $form->getData();
+    /**
+     * @Route("/{slug}")
+     * @Template()
+     * @Method("PUT")
+     *
+     */
+    public function updateAction($slug)
+    {
+        $dm = $this->get('doctrine.odm.mongodb.document_manager');
 
-                $dm = $this->get('doctrine.odm.mongodb.document_manager');
-                $dm->persist($artefact);
-                $dm->flush();
+        $artefact = $dm->getRepository('CbaseCbag3ArtefactBundle:Artefact')->findOneBySlug($slug);
 
-                return $this->redirect($this->generateUrl('cbase_cbag3_artefact_artefact_show', array('id'=> $id)));
-            }
+        if (!$artefact) {
+            throw $this->createNotFoundException('No artefact found for '.$slug);
         }
 
-        return array('form'=>$form->createView(), 'id'=>$id);
+        $form = $this->createForm(new ArtefactType(), $artefact);
+
+        if ($slug = $this->processArtefactForm($form) !== false) {
+            return $this->redirect($this->generateUrl('cbase_cbag3_artefact_artefact_show', array('slug'=> $slug)));
+        }
+        return $this->redirect($this->generateUrl('cbase_cbag3_artefact_artefact_edit', array('slug'=> $slug)));
     }
 
     /**
@@ -117,5 +126,22 @@ class ArtefactController extends Controller
     {
         return $this->get('doctrine.odm.mongodb.document_manager')
             ->getRepository('CbaseCbag3ArtefactBundle:Artefact');
+    }
+
+    protected function processArtefactForm($form)
+    {
+        $form->bind($this->getRequest());
+
+        if ($form->isValid()) {
+            // perform some action, such as saving the task to the database
+            $artefact = $form->getData();
+
+            $dm = $this->get('doctrine.odm.mongodb.document_manager');
+            $dm->persist($artefact);
+            $dm->flush();
+
+            return $artefact->getSlug();
+        }
+        return false;
     }
 }
