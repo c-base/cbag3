75031b50860201a05189b61f7ea6483a76958e02
dazz <dazz@c-base.org>
2012-12-07 21:33:42 +0100

    added removeAsset, removeAssetFromArtefact, removeAssetFromArtefactById and getByAssetId functions

--- src/Cbase/Cbag3/ArtefactBundle/Repository/ArtefactRepository.php
+++ src/Cbase/Cbag3/ArtefactBundle/Repository/ArtefactRepository.php
@@ -3,8 +3,10 @@
 namespace Cbase\Cbag3\ArtefactBundle\Repository;
 
 use Doctrine\ODM\MongoDB\DocumentRepository;
-use Cbase\Cbag3\AssetBundle\Document\Asset;
 use Doctrine\ORM\QueryBuilder;
+
+use Cbase\Cbag3\AssetBundle\Document\Asset;
+use Cbase\Cbag3\ArtefactBundle\Document\Artefact;
 
 /**
  * ArtefactRepository
@@ -14,16 +16,55 @@ use Doctrine\ORM\QueryBuilder;
  */
 class ArtefactRepository extends DocumentRepository
 {
-    public function getByAsset(Asset $asset)
+    public function removeAsset(Asset $asset)
     {
-        $id = $asset->getId();
-        $query = $this->dm->createQueryBuilder();
-        $query->field('asset.$id')->equals($id);
-
-        return $query->getQuery()->execute();
+        return $this->getDocumentManager()->createQueryBuilder()
+            ->update('Cbase\Cbag3\ArtefactBundle\Document\Artefact')
+            ->field('assets')->pull(array(
+                '$id' => new \MongoId($asset->getId()),
+            ))
+            ->multiple(true)
+            ->getQuery()
+            ->execute()
+        ;
     }
 
-    public function getLatest($limit = 5) {
+    public function removeAssetFromArtefact(Asset $asset, Artefact $artefact)
+    {
+        return $this->removeAssetFromArtefactById($asset->getId(), $artefact->getId());
+    }
+
+    public function removeAssetFromArtefactById($assetId, $artefactId)
+    {
+        return $this->getDocumentManager()->createQueryBuilder()
+            ->update('Cbase\Cbag3\ArtefactBundle\Document\Artefact')
+            ->field('_id')->equals(new \MongoId($artefactId))
+            ->field('assets')->pull(array(
+                    '$id' => new \MongoId($assetId),
+                ))
+            ->multiple(true)
+            ->getQuery()
+            ->execute()
+            ;
+    }
+
+    public function getByAsset(Asset $asset)
+    {
+        return $this->getByAssetId($asset->getId());
+    }
+
+    public function getByAssetId($assetId)
+    {
+        return $this->getDocumentManager()->createQueryBuilder()
+            ->find('Cbase\Cbag3\ArtefactBundle\Document\Artefact')
+            ->field('assets.$id')->equals(new \MongoId($assetId))
+            ->getQuery()
+            ->execute()
+        ;
+    }
+
+    public function getLatest($limit = 5)
+    {
         return $this->getDocumentManager()->createQueryBuilder()
             ->find('Cbase\Cbag3\ArtefactBundle\Document\Artefact')
             ->sort('createdAt', 'desc')
@@ -32,20 +73,22 @@ class ArtefactRepository extends DocumentRepository
             ->execute();
     }
 
-    public function getStateStats($stateName) {
-        $qb =  $this->getDocumentManager()->createQueryBuilder();
-
-        return $qb->find('Cbase\Cbag3\ArtefactBundle\Document\Artefact')
+    public function getStateStats($stateName)
+    {
+        return $this->getDocumentManager()->createQueryBuilder()
+            ->find('Cbase\Cbag3\ArtefactBundle\Document\Artefact')
             ->field('state.'.$stateName)->equals(true)
             ->count()
             ->getQuery()
             ->execute();
     }
 
-    public function getByDoesNotHaveThisState($stateName) {
+    public function getByDoesNotHaveThisState($stateName)
+    {
         $qb =  $this->getDocumentManager()->createQueryBuilder();
 
-        return $qb->find('Cbase\Cbag3\ArtefactBundle\Document\Artefact')
+        return $this->getDocumentManager()->createQueryBuilder()
+            ->find('Cbase\Cbag3\ArtefactBundle\Document\Artefact')
             ->addOr($qb->expr()->field('state.'.$stateName)->equals(false))
             ->addOr($qb->expr()->field('state.'.$stateName)->exists(false))
             ->getQuery()
