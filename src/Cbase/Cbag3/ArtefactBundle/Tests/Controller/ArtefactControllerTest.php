72a386d7d4f2417afb26e5ad1edc5ec929fa9bbf
dazz <dazz@c-base.org>
2012-12-15 03:29:36 +0100

    fixed testing

--- src/Cbase/Cbag3/ArtefactBundle/Tests/Controller/ArtefactControllerTest.php
+++ src/Cbase/Cbag3/ArtefactBundle/Tests/Controller/ArtefactControllerTest.php
@@ -19,6 +19,38 @@ class ArtefactControllerTest extends WebTestCase
         $this->assertGreaterThan(0, $crawler->filter('div#artefact_list')->count());
     }
 
+    public function testNew()
+    {
+        $client = $this->createClientWithAuthentication('restricted_area');
+
+        $crawler = $client->request('GET', '/artefact/new');
+
+        $this->assertTrue($crawler->filter('html:contains("artefact anlegen")')->count() > 0);
+        $this->assertTrue($crawler->filter('h1')->count() > 0);
+    }
+
+    public function testCreateNewArtefact()
+    {
+        $client = $this->createClientWithAuthentication('restricted_area');
+
+        $crawler = $client->request('GET', '/artefact/new');
+        $form = $crawler->selectButton('c_peichern')->form();
+
+        $form['artefact[name]'] = 'nerdC_leuder';
+        $form['artefact[description]'] = 'da stand mal ein gyroscop in der c-base. das waren tolle ceiten';
+
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+
+        $this->assertTrue($crawler->filter('html:contains("nerdC_leuder")')->count() > 0);
+
+        $crawler = $client->request('GET', '/artefact/nerdc-leuder');
+        $this->assertTrue($crawler->filter('html:contains("nerdC_leuder")')->count() > 0);
+    }
+
+    /**
+     *
+     */
     public function testClickArtefact()
     {
         $client = static::createClient();
@@ -38,27 +70,4 @@ class ArtefactControllerTest extends WebTestCase
 
         $this->assertGreaterThan(0, $crawler->filter('h1')->count());
     }
-
-    public function testShow()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/artefact/mtc');
-
-        $this->assertTrue($crawler->filter('html:contains("mtc")')->count() > 0);
-    }
-
-    public function testNew()
-    {
-        $client = $this->createClientWithAuthentication('restricted_area');
-
-        $crawler = $client->request('GET', '/artefact/new');
-
-//        print_r($client->getResponse()->getContent());
-
-        $this->assertTrue($crawler->filter('html:contains("artefact anlegen")')->count() > 0);
-        $this->assertTrue($crawler->filter('h1')->count() > 0);
-    }
-
-
 }
