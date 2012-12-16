293264ad81d0825459ae43edb96f4a6148d0b0fe
dazz <dazz@c-base.org>
2012-12-16 03:23:30 +0100

    addded testCreate for assetController

--- src/Cbase/Cbag3/AssetBundle/Tests/Controller/AssetControllerTest.php
+++ src/Cbase/Cbag3/AssetBundle/Tests/Controller/AssetControllerTest.php
@@ -2,16 +2,35 @@
 
 namespace Cbase\Cbag3\AssetBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Cbase\Cbag3\BaseBundle\Test\WebTestCase;
 
 class AssetControllerTest extends WebTestCase
 {
+    public function testCreate()
+    {
+        $client = $this->createClientWithAuthentication('restricted_area');
+        $crawler = $client->request('GET', '/asset/new');
+        $form = $crawler->selectButton('c_peichern')->form();
+
+        $image = __DIR__.'/../Resources/public/images/are_you_happy.jpg';
+
+        $form['artefact_asset[description]'] = 'are you happy';
+        $form['artefact_asset[author]'] = 'mr. testuser himself';
+        $form['artefact_asset[licence]']->select('CC-BY-SA');
+        $form['artefact_asset[file]']->upload($image);
+
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+
+        $this->assertTrue($crawler->filter('html:contains("neue grafic wurde in den speicher aufgenommen")')->count() > 0);
+    }
+
     public function testIndex()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/asset/');
 
-        $this->assertTrue($crawler->filter('html:contains("grafic_e darstellungen")')->count() > 0);
+        $this->assertEquals("grafic_e darstellungen", $crawler->filter('h1')->text());
     }
 }
