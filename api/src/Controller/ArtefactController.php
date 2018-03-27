02c9e34b333264c91b4180ae5e01459490ab7489
Anne-Julia Seitz <annejulia@gmail.com>
2018-03-27 23:34:27 +0200

    Controller: Entrypoint and Artefact

--- api/src/Controller/ArtefactController.php
+++ api/src/Controller/ArtefactController.php
@@ -1,31 +1,102 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Artefact;
 use App\Utils\Slugger;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * @Route("/artefacts")
  */
 class ArtefactController extends AbstractController
 {
+    /** @var UrlGeneratorInterface */
+    private $urlGenerator;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository  */
+    private $artefactRepository;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator, EntityManagerInterface $entityManager)
+    {
+        $this->urlGenerator = $urlGenerator;
+        $this->artefactRepository = $entityManager->getRepository(Artefact::class);
+    }
+
     /**
      * @Route("/", name="artefact.collection", methods={"GET"})
      */
     public function collection()
     {
-        return new Response('collection');
+        /** @var Artefact[] $artefacts */
+        $artefacts = $this->artefactRepository->findAll();
+
+        $content = [
+            "@context" => "http://www.w3.org/ns/hydra/context.jsonld",
+            "@id" => $this->urlGenerator->generate('artefact.collection', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            "@type" => "Collection",
+            "title" => "All collected artefacts of c-base",
+            'totalItems' => count($artefacts),
+            "artefacts" => [],
+            "supportedOperation" => [
+                [
+                    "@type" => "Operation",
+                    "title" => "Creates a new artefact",
+                    "method" => "POST",
+                    "expects" => "Artefact",
+                    "returns" => "Artefact",
+                    "possibleStatus" => [
+                        Response::HTTP_CREATED => 'Successful created'
+                    ]
+                ]
+            ]
+        ];
+
+        foreach ($artefacts as $artefact) {
+            $content['artefacts'][] = [
+                '@id' => $this->urlGenerator->generate('artefact.item', ['slug' => $artefact->getSlug()], UrlGeneratorInterface::ABSOLUTE_URL),
+                '@type' => 'Class',
+                'name' => $artefact->getName(),
+                'createdAt' => $artefact->getCreatedAt()->format('Y-m-d'),
+                'createdBy' => $artefact->getCreatedBy(),
+            ];
+        }
+
+        return $this->json(
+            $content,
+            Response::HTTP_OK,
+            ['Content-Type' => 'application/ld+json; charset=utf-8']
+        );
     }
 
     /**
      * @Route("/{slug}", name="artefact.item", methods={"GET"})
+     * @ParamConverter("artefact")
      */
-    public function item()
+    public function item(Artefact $artefact)
     {
-        return new Response('item');
+        $content = [
+            '@id' => $this->urlGenerator->generate('artefact.item', ['slug' => $artefact->getSlug()], UrlGeneratorInterface::ABSOLUTE_URL),
+            '@type' => 'Class',
+            'name' => $artefact->getName(),
+            'description' => $artefact->getDescription(),
+            'createdAt' => $artefact->getCreatedAt()->format('Y-m-d'),
+            'createdBy' => $artefact->getCreatedBy(),
+            'assets' => [
+                '@type' => 'Collection',
+            ]
+        ];
+
+        return $this->json(
+            $content,
+            Response::HTTP_OK,
+            ['Content-Type' => 'application/ld+json; charset=utf-8']
+        );
     }
 
     /**
@@ -33,6 +104,10 @@ class ArtefactController extends AbstractController
      */
     public function create(Request $request, Slugger $slugger)
     {
-        return new Response();
+        return $this->json(
+            'item',
+            Response::HTTP_CREATED,
+            ['Content-Type' => 'application/ld+json; charset=utf-8']
+        );
     }
 }
