29d3d10cc8c40e439c3591a34c929096acbd7685
Anne-Julia Seitz <annejulia@gmail.com>
2018-04-02 21:41:00 +0200

    Exceptions: log all errors with different severity

--- api/src/EventListener/ExceptionListener.php
+++ api/src/EventListener/ExceptionListener.php
@@ -8,6 +8,7 @@
 
 namespace App\EventListener;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
@@ -19,11 +20,21 @@ use Symfony\Component\Security\Core\Exception\InsufficientAuthenticationExceptio
 
 class ExceptionListener
 {
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         $content = [
             'status' => 0,
-            'trace' => explode("\n", $event->getException()->getTraceAsString()),
         ];
         $response = new JsonResponse();
 
@@ -35,13 +46,18 @@ class ExceptionListener
 
             $content['status'] = $exception->getStatusCode();
             $content['error'] = $exception->getMessage();
+
+            $this->logger->debug($content['error']);
         } else if ($this->isUnauthorized($exception)) {
             $response->setStatusCode(Response::HTTP_UNAUTHORIZED);
             $content['status'] = Response::HTTP_UNAUTHORIZED;
             $content['error'] = 'Unauthorized';
+
+            $this->logger->info($content['error']);
         } else {
             $content['status'] = Response::HTTP_INTERNAL_SERVER_ERROR;
             $content['error'] = 'Server Error';
+            $this->logger->error($content['error'], $event->getException()->getTrace());
         }
 
         $response->setContent(json_encode($content));
