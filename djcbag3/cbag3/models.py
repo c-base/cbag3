ecf8175ddcf673da5e2cb4e899515641e5e57386
cmile <github@cmile.de>
2020-12-25 22:33:39 +0100

    update models

--- djcbag3/cbag3/models.py
+++ djcbag3/cbag3/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Artefact(models.Model):
+    id = models.TextField()
+    name = models.CharField(max_length=30)
+    slug = models.CharField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField()
+    # assets
