11a1d93a8c42040d936fe627a213b7167479feca
cmile <github@cmile.de>
2020-12-25 22:53:49 +0100

    update model

--- cbag3/models.py
+++ cbag3/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class Artefact(models.Model):
     id = models.TextField()
-    name = models.CharField(max_length=30)
-    slug = models.CharField()
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.CharField()
+    created_by = models.CharField(max_length=255)
     # assets
