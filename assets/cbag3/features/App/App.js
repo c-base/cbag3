a338d9478941588460b3dcbd5a13c298efc13d02
dazz <dazz@c-base.org>
2020-12-26 20:29:55 +0100

    App: added react-router with first route

--- assets/cbag3/features/App/App.js
+++ assets/cbag3/features/App/App.js
@@ -1,14 +1,44 @@
 import React from 'react'
 
 import {Row} from 'react-bootstrap'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 import './App.css'
 
 const App = () => (
+  <Router>
     <div>
-      <Row id="h" className="mt-2">Hello</Row>
-      <Row id="e" className="mt-2">World</Row>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">CBAG3 - c-base artefact guide 3.2</Link>
+          </li>
+        </ul>
+      </nav>
+      
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
+  </Router>
 )
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 
 export default App
