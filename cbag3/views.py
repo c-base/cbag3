070d3acf2092b22fb5403d3293c867ba1dc5e57b
cmile <github@cmile.de>
2020-12-29 21:38:39 +0100

    exted state

--- cbag3/views.py
+++ cbag3/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.shortcuts import render
 from django.views import View
+from django.middleware.csrf import get_token
 
 from cbag3.models import Artefact
 
@@ -10,10 +11,18 @@ class Home(View):
     template_name = 'home.html'
 
     def get(self, request):
+        is_authenticated = False
+        username = None
+        if request.user.is_authenticated():
+            is_authenticated = True
+            username = request.user.username
         context = {
             'state': json.dumps({
                 'config': {},
-                'artefacts': [artefact.as_dict() for artefact in Artefact.objects.all()]
+                'csrfToken': get_token(request),
+                'username': username,
+                'isAuthenticated': is_authenticated,
+                'artefacts': [artefact.as_dict() for artefact in Artefact.objects.all()],
             })
         }
         return render(request, self.template_name, context)
