bfe69c1b5cc698d99d77f23d5287848828cae774
Anne-Julia Seitz <annejulia@gmail.com>
2021-10-10 17:55:45 +0200

    Setup: Add annotations and first Controller/Action

---
+++ src/Home/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Home;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
