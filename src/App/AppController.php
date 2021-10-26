3bbb95ee5cba899f92c53d5981b2fce4eb414097
Anne-Julia Seitz <annejulia@gmail.com>
2021-10-26 23:02:36 +0200

    App: get config injected

--- src/App/AppController.php
+++ src/App/AppController.php
@@ -5,6 +5,7 @@ namespace App\App;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 class AppController extends AbstractController
 {
@@ -14,15 +15,36 @@ class AppController extends AbstractController
     public function index(): Response
     {
         $config = [
-            'resources' => [
-                'artefacts' => [
-                    'path' => $this->generateUrl('artefact_collection'),
-                    'methods' => 'get'
-                    ],
-            ]
+            'user' => null,
+            'resources' => $this->getResources(),
         ];
         return $this->render('app/index.html.twig', [
             'config' => json_encode($config),
         ]);
+    }
+
+    /**
+     * @return array
+     */
+    private function getResources(): array
+    {
+        /** @var RouterInterface $router */
+        $router = $this->container->get('router');
+        $routes = $router->getRouteCollection()->all();
+        $routes = array_filter($routes, function ($routeName) {
+            if (str_starts_with($routeName, '_') || str_starts_with($routeName, 'admin')) {
+                return false;
+            }
+            return true;
+        }, ARRAY_FILTER_USE_KEY);
+
+        array_walk($routes, function(&$route, $routeName) {
+            $route = [
+                'name' => $routeName,
+                'path' => $route->getPath(),
+                'methods' => $route->getMethods(),
+            ];
+        });
+        return $routes;
     }
 }
