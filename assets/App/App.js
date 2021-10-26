771f562a8540f2281930f508500955a3f7e276c5
Anne-Julia Seitz <annejulia@gmail.com>
2021-10-26 23:03:30 +0200

    Frontend: setup the app

--- assets/App/App.js
+++ assets/App/App.js
@@ -1,14 +1,41 @@
-import React, { Component} from "react";
-import "./App.css";
+import React from "react"
+import {Navbar, Nav, Container, CardGroup, Card, Row} from "react-bootstrap";
+import "./App.css"
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">CBRP3 : artefactguide</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#artefacts">Artefacts</Nav.Link>
+            <Nav.Link href="#gallery">Gallery</Nav.Link>
+            <Nav.Link href="#login">Login</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Container>
+        <Row>&nbsp;</Row>
+        <Row>
+        <CardGroup>
+          <Card bg={"dark"}>
+            <Card.Img variant="top" src="holder.js/100px160?theme=industrial" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </CardGroup>
+        </Row>
+      </Container>
+    </div>
+)
 
 export default App;
--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'holderjs'
+
 import './index.css';
 
 import App from "./App/App.js";
